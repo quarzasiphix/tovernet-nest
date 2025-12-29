f7b6ddd40a22f975ec7fb9f6af36feab8700fdeb
quarzasiphix <76133968+quarzasiphix@users.noreply.github.com>
2025-12-29 12:55:37 +0100

    latest

---
+++ next-intl.config.ts
@@ -0,0 +1,2 @@
+export { default } from './i18n';
+export { locales } from './i18n';
--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "export",
-  images: { unoptimized: true }, // avoids next/image export issues
+  images: { unoptimized: true },
+  trailingSlash: true,
 };
