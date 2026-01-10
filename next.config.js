fdd0693f7b50796e8f2e0b420b1acba77ee56c7e
quarzasiphix <quarrysiphix@gmail.com>
2026-01-10 01:12:57 +0100

    latest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withNextIntl({
   output: "export",
   images: { unoptimized: true },
   trailingSlash: true,
-};
+});
